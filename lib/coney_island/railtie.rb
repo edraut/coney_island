01d877b061d48356d5e2ba25002a88ad2446b9fa
Eric Draut <edraut@gmail.com>
2015-01-26 20:31:31 -0700

    don't try to add an activejob adapter if activejob ain't thar

--- lib/coney_island/railtie.rb
+++ lib/coney_island/railtie.rb
@@ -2,7 +2,9 @@ require 'coney_island/coney_island_adapter'
 module ConeyIsland
   class Railtie < Rails::Railtie
     initializer "coney_island.coney_island_adapter" do
-      ActiveJob::QueueAdapters.send :autoload, :ConeyIslandAdapter
+      if defined? ActiveJob
+        ActiveJob::QueueAdapters.send :autoload, :ConeyIslandAdapter
+      end
     end
   end
 end
