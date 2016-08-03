c55e21b150e58c19e5ca3a37bd168d31690a8368
Leonardo Bighetti <leonardo.bighetti@gmail.com>
2016-08-03 20:18:08 -0300

    deal with leaky tests

--- test/performer_test.rb
+++ test/performer_test.rb
@@ -105,8 +105,16 @@ class PerformerTest < MiniTest::Test
   end
 
   describe 'highlander option' do
-    before { ConeyIsland.cache_jobs        }
-    after  { ConeyIsland.flush_jobs; ConeyIsland.stop_caching_jobs }
+
+    def setup
+      ConeyIsland.flush_jobs # flush before doing anything to prevent leaks
+      ConeyIsland.cache_jobs
+    end
+
+    def teardown
+      ConeyIsland.flush_jobs
+      ConeyIsland.stop_caching_jobs
+    end
 
     it "is understood by performers" do
       5.times { MyHighlander.increment_async }
