f14ac5201b8a3e27339d664c74739c9aae0e44ee
Leonardo Bighetti <leonardo.bighetti@gmail.com>
2016-12-07 21:35:23 -0200

    fix activejob adapter for rails 5

--- lib/coney_island/coney_island_adapter.rb
+++ lib/coney_island/coney_island_adapter.rb
@@ -9,25 +9,21 @@ module ActiveJob
       # To use ConeyIsland set the queue_adapter config to +:coney_island+.
       #
       #   Rails.application.config.active_job.queue_adapter = :coney_island
-      class << self
-        def enqueue(job) #:nodoc:
-          ConeyIsland::Submitter.submit JobWrapper, :perform, args: [job.serialize], work_queue: job.queue_name, timeout: get_timeout_from_args(job), retry_limit: get_retry_from_args(job)
-        end
+      def enqueue(job) #:nodoc:
+        ConeyIsland::Submitter.submit JobWrapper, :perform, args: [job.serialize], work_queue: job.queue_name, timeout: get_timeout_from_args(job), retry_limit: get_retry_from_args(job)
+      end
 
-        def enqueue_at(job, timestamp) #:nodoc:
-          delay = timestamp - Time.current.to_f
-          ConeyIsland::Submitter.submit JobWrapper, :perform, args: [job.serialize], work_queue: job.queue_name, delay: delay, timeout: get_timeout_from_args(job), retry_limit: get_retry_from_args(job)
-        end
+      def enqueue_at(job, timestamp) #:nodoc:
+        delay = timestamp - Time.current.to_f
+        ConeyIsland::Submitter.submit JobWrapper, :perform, args: [job.serialize], work_queue: job.queue_name, delay: delay, timeout: get_timeout_from_args(job), retry_limit: get_retry_from_args(job)
+      end
 
-        def get_timeout_from_args(job)
-          job.class::TIMEOUT if job.class.const_defined? :TIMEOUT
-        end
+      def get_timeout_from_args(job)
+        job.class::TIMEOUT if job.class.const_defined? :TIMEOUT
+      end
 
-        def get_retry_from_args(job)
-          job.class::RETRY_LIMIT if job.class.const_defined? :RETRY_LIMIT
-        end
-
-
+      def get_retry_from_args(job)
+        job.class::RETRY_LIMIT if job.class.const_defined? :RETRY_LIMIT
       end
 
       class JobWrapper #:nodoc:
